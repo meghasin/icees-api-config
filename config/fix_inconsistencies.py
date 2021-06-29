81b259f818ceeee3de005f9fa82f28ff6867c3d0
Meghamala Sinha <meghamala.sinha@gmail.com>
2021-06-29 08:00:27 -0700

    added complete list of inconsistent identifiers

--- config/fix_inconsistencies.py
+++ config/fix_inconsistencies.py
@@ -2,16 +2,17 @@ import yaml
 import json
 import requests
 import csv
+import pandas as pd
 
 def checkIdExits(ID):
-    NODE_NORMALIZER_QUERY_URL = "https://nodenormalization-sri.renci.org/1.1/get_normalized_nodes"
-    idExists = None
+    node_normalizer_url = "https://nodenormalization-sri.renci.org/1.1/get_normalized_nodes"
+    id_exists = None
     input_obj = {
         "curies": [ID]
     }
-    curl_cmd = f"curl -XPOST {NODE_NORMALIZER_QUERY_URL} -H \"Content-Type: application/json\" -d '{json.dumps(input_obj)}'"
+    curl_cmd = f"curl -XPOST {node_normalizer_url} -H \"Content-Type: application/json\" -d '{json.dumps(input_obj)}'"
 
-    resp = requests.post(NODE_NORMALIZER_QUERY_URL, headers={
+    resp = requests.post(node_normalizer_url, headers={
         "Content-Type": "application/json",
         "Accept": "applicaton/json"
     }, json=input_obj)
@@ -19,22 +20,33 @@ def checkIdExits(ID):
     obj = resp.json()
     # print(obj)
     for key, value in obj.items():
-        idExists = value
-        # print(idExists)
-    return idExists
+        id_exists = value
+        # print(id_exists)
+    return id_exists
 
-def check_inconsistency(document):
+def extract_identifiers(document):
+    a = []
+    for table, feature in document.items():
+        for feature_name, identifier_list in feature.items():
+            # print(table, ":", feature_name, ":", identifier_list)
+            for identifier in identifier_list:
+                a.append(identifier)
+    print(len(a))
+    a = pd.unique(a).tolist()
+    print(len(a))
+    return a
+
+def check_inconsistency(identifiers):
     with open("inconsistencies.csv", 'w') as f:
-        writer = csv.writer(f, delimiter='\t', lineterminator='\n',)
+        writer = csv.writer(f, delimiter='\t', lineterminator='\n', )
         for table, feature in document.items():
             for feature_name, identifier_list in feature.items():
-                # print(table, ":", feature_name, ":", identifier_list)
                 for identifier in identifier_list:
-                    # print(identifier)
                     if not checkIdExits(identifier):
                         print(identifier)
                         writer.writerow(iter(identifier))
 
 with open(r'identifiers.yml') as file:
     document = yaml.full_load(file)
-check_inconsistency(document)
+identifiers = extract_identifiers(document)
+check_inconsistency(identifiers)
