2b77e3a61619f0784ce4048d363807529b2830d3
Meghamala Sinha <meghamala.sinha@gmail.com>
2021-07-05 13:18:38 -0700

    adding updated yaml file

--- config/fix_inconsistencies.py
+++ config/fix_inconsistencies.py
@@ -3,6 +3,9 @@ import json
 import requests
 import csv
 import pandas as pd
+from tempfile import NamedTemporaryFile
+import shutil
+
 
 def checkIdExits(ID):
     node_normalizer_url = "https://nodenormalization-sri.renci.org/1.1/get_normalized_nodes"
@@ -24,6 +27,7 @@ def checkIdExits(ID):
         # print(id_exists)
     return id_exists
 
+
 def extract_identifiers(document):
     a = []
     for table, feature in document.items():
@@ -36,17 +40,39 @@ def extract_identifiers(document):
     print(len(a))
     return a
 
+
 def check_inconsistency(identifiers):
     with open("inconsistencies.csv", 'w') as f:
-        writer = csv.writer(f, delimiter='\t', lineterminator='\n', )
-        for table, feature in document.items():
-            for feature_name, identifier_list in feature.items():
-                for identifier in identifier_list:
-                    if not checkIdExits(identifier):
-                        print(identifier)
-                        writer.writerow(iter(identifier))
+        writer = csv.writer(f, lineterminator='\n')
+        for item in identifiers:
+            if not checkIdExits(item):
+                print(item)
+                writer.writerow([item])
+
+def update_identifiers(document):
+    b = []
+    filename = 'inconsistencies.csv'
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            row = str(row)[2:-2]
+            b.append(row)
+            print(row)
+    #print(b)
+    for table, feature in document.items():
+        for feature_name, identifier_list in feature.items():
+            #print(table, ":", feature_name, ":", identifier_list)
+            for identifier in identifier_list:
+                #print(identifier)
+                if identifier in b:
+                    #print(identifier)
+                    identifier_list.remove(identifier)
+    return document
 
 with open(r'identifiers.yml') as file:
     document = yaml.full_load(file)
-identifiers = extract_identifiers(document)
-check_inconsistency(identifiers)
+#identifiers = extract_identifiers(document)
+#check_inconsistency(identifiers)
+d = update_identifiers(document)
+with open(r'consistent_identifiers.yml', 'w') as file:
+    documents = yaml.dump(d, file)
